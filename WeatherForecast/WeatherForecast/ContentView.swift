bb2a08571040cec64b3afe7a25eb8c690b4382c8
Sirojiddin Kuzmonboev <49828168+niddijoris@users.noreply.github.com>
2024-10-17 17:03:53 +0500

    first project

---
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  WeatherForecast
+//
+//  Created by Sirojiddin Kuzmonboev on 17/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct DayForecast: View {
+    let day:String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)º")
+                .fontWeight(Font.Weight.semibold)
+            Text("Low: \(low)º")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
+        }
+        .padding()
+    }
+}
