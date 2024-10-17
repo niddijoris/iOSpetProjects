e006307d05d096fdee6618ca33187a34a1328dba
Sirojiddin Kuzmonboev <49828168+niddijoris@users.noreply.github.com>
2024-10-17 17:04:50 +0500

    Third project

---
+++ ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Sirojiddin Kuzmonboev on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
