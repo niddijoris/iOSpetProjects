c047920775c4808a72d2b7e44041c9d18f1e7edf
Sirojiddin Kuzmonboev <49828168+niddijoris@users.noreply.github.com>
2024-12-04 17:54:39 +0500

    Add files via upload
    5- project

---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Sirojiddin Kuzmonboev on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
