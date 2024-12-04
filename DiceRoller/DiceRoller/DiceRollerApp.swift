c0b4cfdc41393ad290019652d62d332fe44397fd
Sirojiddin Kuzmonboev <49828168+niddijoris@users.noreply.github.com>
2024-12-04 17:57:14 +0500

    6-project

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Sirojiddin Kuzmonboev on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
